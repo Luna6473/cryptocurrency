65309915be7efd6e8f1c3bb456a3d10934e0f989
Luna6473 <ra1295@srmist.edu.in>
2025-03-09 16:33:29 +0530

    commit 16

--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -2,8 +2,26 @@ import React from 'react'
 import "./styles.css";
 function Header() {
   return (
-    <div className="header">Header</div>
-  )
+    <div className="navbar">
+      <h1 className="logo">
+        cryptoTracker<span style={{color: "var(--blue)"}}>.</span>
+      </h1>
+      <div className="links">
+        <a href="/">
+          <p className="links">Home</p>
+        </a>
+        <a href="/">
+          <p className="links">Compare</p>
+        </a>
+        <a href="/">
+          <p className="links">Watchlist</p>
+        </a>
+        <a href="/">
+          <p className="links">Dashboard</p>
+        </a>
+      </div>  
+    </div>
+  );
 }
 
 export default Header
